1e85ab8908e40b68c3efc92c71991ef48ca5970f
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-17 16:03:25 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/MainActivity.java
@@ -0,0 +1,38 @@
+package kr.co.tjoeun.a20200317_03_androidfunction01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+
+import kr.co.tjoeun.a20200317_03_androidfunction01.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.goToSecondBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+    }
+}
+
+
+
