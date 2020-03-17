306270074a0f66ad72f6b5f4175f55dc0de92a81
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-17 16:04:11 +0900

    버튼 두개 생성 이벤트 준비

--- app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         binding.goToSecondBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
